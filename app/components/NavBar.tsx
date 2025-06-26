acca96709a1003cd6322f99ba1e283385153c0f3
Nasar <108045899+nasar-17@users.noreply.github.com>
2025-06-27 01:33:59 +0800

    Update NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,7 +4,7 @@ function NavBar() {
   return (
 	<nav className="bg-blue-500 p-4">
 	  <div className="container mx-auto flex justify-between items-center">
-		<div className="text-white text-xl font-bold">MyWebsite</div>
+		<div className="text-white text-xl font-bold">NasarWeb</div>
 		<ul className="flex space-x-4">
 		  <li>
 			<a href="#" className="text-white hover:text-gray-200">
